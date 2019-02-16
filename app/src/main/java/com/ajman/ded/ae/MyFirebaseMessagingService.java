b3b8efefa320675b7ba7cac8b61c5f45f23fbd9e
AhmedMaher07 <Ahmed.Maher07@hotmail.com>
2019-02-16 19:18:26 +0200

    final

--- app/src/main/java/com/ajman/ded/ae/MyFirebaseMessagingService.java
+++ app/src/main/java/com/ajman/ded/ae/MyFirebaseMessagingService.java
@@ -1,5 +1,6 @@
 package com.ajman.ded.ae;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -7,17 +8,13 @@ import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.util.Log;
 import android.widget.RemoteViews;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
 import java.util.Locale;
-import java.util.Random;
-
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     String channelId = "1";
@@ -25,6 +22,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private NotificationManager notificationManager;
 
     private static final String TAG = "MyFirebaseMsgService";
+
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
@@ -39,25 +37,26 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         contentView.setTextViewText(R.id.notification_no, String.format("%s %s", getString(R.string.notification_no), remoteMessage.getData().get("RequestNumber"), Locale.forLanguageTag("ar")));
         contentView.setTextViewText(R.id.content, String.format("%s %s %s", remoteMessage.getData().get("message") ,getString(R.string.notification_content),remoteMessage.getData().get("EstablishmentNameAR"), Locale.forLanguageTag("ar")));
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+        Notification notification = new NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setStyle(new NotificationCompat.DecoratedCustomViewStyle())
                 .setCustomContentView(contentView)
                 .setCustomContentView(contentView)
                 .setCustomHeadsUpContentView(contentView)
+                .setColor(ContextCompat.getColor(this,R.color.colorAccent))
                 .setAutoCancel(true)
                 .setWhen(when)
-                .setSound(defaultSoundUri);
+                .setSound(defaultSoundUri)
+                .build();
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(1, notificationBuilder.build());
+        notificationManager.notify(1, notification);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setupChannels() {
-
         NotificationChannel adminChannel;
         adminChannel = new NotificationChannel(channel2, "DedEye", NotificationManager.IMPORTANCE_HIGH);
         adminChannel.enableLights(true);
